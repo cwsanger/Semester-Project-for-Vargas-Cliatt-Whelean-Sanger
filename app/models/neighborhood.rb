8fdf74fa00d4025ef377e18e426807d9b7736a32
cwhelan00 <chrismwhelan95@gmail.com>
2016-03-01 19:40:09 -0500

    adds slugs to neighborhoods

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,4 +2,14 @@ class Neighborhood < ActiveRecord::Base
   has_many :users
 
   validates :name, presence: true, uniqueness: true
+  after_validation :create_slug
+
+  def to_param
+    slug
+  end
+
+  private
+    def create_slug
+      self.slug = name.parameterize
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301231113) do
+ActiveRecord::Schema.define(version: 20160302002710) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "type"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160301231113) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "slug"
   end
 
   create_table "posts", force: :cascade do |t|
