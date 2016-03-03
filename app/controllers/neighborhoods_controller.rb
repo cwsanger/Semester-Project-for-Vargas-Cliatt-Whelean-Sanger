4945e0c898320727ad6d9f133ea432201b535554
cwhelan00 <chrismwhelan95@gmail.com>
2016-03-03 14:10:58 -0500

    correctly orders posts in neighborhood page

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -10,7 +10,10 @@ class NeighborhoodsController < ApplicationController
   # GET /neighborhoods/neighborhood-slug
   # GET /neighborhoods/neighborhood-slug.json
   def show
-    @posts = Post.joins(:user).where(users: { neighborhood_id: @neighborhood.id })
+    @posts = Post.joins(:user)
+                 .where(users: { neighborhood_id: @neighborhood.id })
+                 .order(created_at: :desc)
+
     @alerts = Alert.all
     @broadcasts = Broadcast.all
     @advertisements = Advertisement.all
