007cba2f95b784e7011e08aacc9183f1e97726ad
cwhelan00 <chrismwhelan95@gmail.com>
2016-04-03 19:04:27 -0400

    adds pundit policy for direct messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   include Pundit
-
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   protect_from_forgery with: :exception
 
   before_action :authentication
@@ -20,5 +20,10 @@ class ApplicationController < ActionController::Base
       else
         redirect_to login_url, notice: 'Log in required'
       end
+    end
+
+  private
+    def user_not_authorized
+      redirect_to(request.referrer || root_path)
     end
 end
