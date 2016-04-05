bec0de5c68b58535107a441d17880242ac04d6f5
Matthew Cliatt <mtcliatt@valdosta.edu>
2016-04-05 12:40:38 -0400

    search works, submit button on next line though

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     get 'start' => :start
     post 'start' => :post_start
     get 'search' => :search
+    post 'search' => :search
     get 'about' => :about
     get 'contact' => :contact
     get 'doc' => :doc
