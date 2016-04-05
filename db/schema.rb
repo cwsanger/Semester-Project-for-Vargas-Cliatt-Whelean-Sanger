da4b733e3bc0cd32c6f22152a3d18d4c36d33266
Christopher Sanger <cwsanger@valdosta.edu>
2016-04-05 03:44:33 -0400

    Quick change to a placeholder

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160402041048) do
+ActiveRecord::Schema.define(version: 20160404173037) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "email"
