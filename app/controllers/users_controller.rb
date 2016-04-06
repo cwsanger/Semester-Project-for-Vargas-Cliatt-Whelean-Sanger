b45d347c6005845dd0df90eb61c94ca250080873
Matthew Cliatt <mtcliatt@valdosta.edu>
2016-04-06 01:05:01 -0400

    User edit page complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,15 +41,42 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+
+    """
+        We don't know the name of the parameter that needs to be updated
+        so updateParam is also passed to this method and its value is
+        the name of that unknown parameter.
+
+        params[:updateParam] gives us the string value of the name of the
+        unknown parameter and so we can use that to get value of the unknown
+        parameter with params[ params[:updateParam] ]
+
+    """
+
+    paramName = params[:updateParam]
+    paramValue = params[ paramName ]
+
+      #Make sure the parameter to be updated is one we have whitelisted
+      if ['name', 'email', 'address'].include? paramName and not paramName.empty?
+
+        if paramName == 'email'
+          @current_member.account.update_attribute(paramName, paramValue)
+          notice = 'good job, admin was updated'
+        else
+          @current_member.update_attribute(paramName, paramValue)
+          notice = 'good job, user was updated'
+        end
+      elsif paramValue.empty?
+        notice = 'Your ' + paramName + ' can not be blank.'
       else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        notice = 'Unexpected parameter'
       end
-    end
+
+      respond_to do |format|
+        format.html { redirect_to edit_user_path(@current_member), notice: notice }
+        format.json { render :edit, status: :ok, location: @current_member }
+      end
+
   end
 
   # DELETE /users/1
@@ -70,6 +97,10 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :name, :password, :password_confirmation)
+      params.require(:user).permit(:updateParam, :email, :name, :password, :password_confirmation)
+    end
+
+    def account_params
+      params.require(:account).permit(:updateParam, :email)
     end
 end
