31c902d8f11c9e983c95524e9b840d54ddd60cfa
Matthew Cliatt <mtcliatt@valdosta.edu>
2016-04-20 13:06:54 -0400

    Account.setup(email) creates account for email and emails user

---
+++ app/mailers/account_notifier.rb
@@ -0,0 +1,29 @@
+class AccountNotifier < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.account_notifier.created.subject
+  #
+  def created(account)
+    @greeting = "Hi"
+
+    mail to: account.email, subject: 'Account created' do |format|
+      format.html
+    end
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.account_notifier.updated.subject
+  #
+  def updated(account)
+    @greeting = "Hi"
+
+    mail to: account.email, subject: 'Account was updated' do |format|
+      format.html
+    end
+
+  end
+end
