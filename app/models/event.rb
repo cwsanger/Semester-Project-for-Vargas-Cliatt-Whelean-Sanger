bfc4ea26e3ccd1164c39ac434537621d58f55505
Christopher Sanger <cwsanger@valdosta.edu>
2016-04-25 03:10:11 -0400

    Maps works, images in events dont

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,12 @@
 class Event < ActiveRecord::Base
   belongs_to :neighborhood
   belongs_to :user
+
+  geocoded_by :address
+  after_validation :geocode
+
+  acts_as_mappable lat_column_name: :latitude,
+                   lng_column_name: :longitude
+
   mount_uploader :image_url, PictureUploader
 end
