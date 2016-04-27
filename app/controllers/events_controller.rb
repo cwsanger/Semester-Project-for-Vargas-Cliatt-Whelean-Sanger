40123c5190029403329b0ed7c5ad6b2ae94a5faa
Christopher Sanger <cwsanger@valdosta.edu>
2016-04-27 01:28:18 -0400

    Events are properly displayed. Modal for creating events rearranged. Seeded data changed.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @events = @current_member.neighborhood.events
     @chosen_date = Integer(Time.now.strftime("%d"))
     @event = Event.new
   end
